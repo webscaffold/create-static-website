37119730fa40a412ca68d244d981d467e02767e1
Radu Micu <radu.micu@gmail.com>
2019-10-31 14:41:14 +0000

    Separate modular class name from eleventy config file

--- .eleventy.js
+++ .eleventy.js
@@ -1,47 +1,6 @@
-const fs = require('fs');
 const nunjucks = require('nunjucks');
+const ModularClassName = require('./lib/modular-class-name');
 const { path: sitePath } = require('./site.config');
-
-class ModularClassName {
-	constructor(output) {
-		this._output = output;
-		this._cache = new Map();
-	}
-	_getData(css) {
-		if (!css.startsWith('/')) {
-			throw new TypeError('CSS path must be absolute (starts with /)');
-		}
-
-		if (!this._cache.has(css)) {
-			const file = this._output + css + '.json';
-			const json = fs.readFileSync(file, {
-				encoding: 'utf8'
-			});
-			this._cache.set(css, JSON.parse(json));
-		}
-
-		return this._cache.get(css);
-	}
-	getClassName(css, className) {
-		const data = this._getData(css);
-
-		if (!(className in data)) {
-			throw new TypeError(`Cannot find className "${className}" in ${css}`);
-		}
-
-		return data[className];
-	}
-	getAllCamelCased(css) {
-		const output = {};
-		const data = this._getData(css);
-
-		for (const [key, val] of Object.entries(data)) {
-			output[key.replace(/-\w/g, (match) => match[1].toUpperCase())] = val;
-		}
-
-		return output;
-	}
-}
 
 module.exports = function(eleventyConfig) {
 	const config = {
